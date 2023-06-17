133de255bb5d44577c2eb5f05465583a29ea66a2
radek-nowak <radeknowak1999@gmail.com>
2023-06-17 23:20:33 +0200

    feature: add components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {ButtonComponent} from './components/button/button.component';
-import {TasksComponent} from './components/tasks/tasks.component';
-import {TaskItemComponent} from './components/task-item/task-item.component';
+import {TasksComponent} from './components/tasks/tasks.component'
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {HttpClientModule} from '@angular/common/http';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import {HttpClientModule} from '@angular/common/http';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
